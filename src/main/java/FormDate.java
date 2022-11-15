6e04d34e815ee0114c9d8fd3aea840da2121f157
Liliya Yapparova <yapparovalili@yandex.ru>
2022-11-15 11:45:50 +0700

    Add code and tests

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,10 @@
+public class FormDate {
+
+
+    public int day;
+
+    public int month;
+
+    public int year;
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,11 @@
+public class Post {
+
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+
+}
